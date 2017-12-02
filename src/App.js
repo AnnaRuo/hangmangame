3b1b54a1d306cfd1f72eb63858eb41229cc97925
Anna <anna.ruotanen@gmail.com>
2017-12-02 11:01:25 +0100

    Create GuessWord component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GuessLetter from './components/GuessLetter'
+import GuessWord from './components/GuessWord'
 
 class App extends Component {
   render() {
