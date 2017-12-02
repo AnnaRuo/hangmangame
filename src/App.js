43959740364e14d8a6c5cd673d2b715f32f2777c
Anna <anna.ruotanen@gmail.com>
2017-12-02 11:00:13 +0100

    Create GuessLetter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GuessLetter from './components/GuessLetter'
 
 class App extends Component {
   render() {
